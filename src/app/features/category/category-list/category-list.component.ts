a047c9f3821f3456bcc35fcb2f594aafefd2ab36
MounikaMalreddy <mounikamalreddy1998@gmail.com>
2025-07-04 19:08:34 +0530

    added filtering,sorting,pagination 04072025

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -8,18 +8,40 @@ import { Observable } from 'rxjs';
   selector: 'app-category-list',
   imports: [CommonModule, RouterModule],
   templateUrl: './category-list.component.html',
-  styleUrl: './category-list.component.css'
+  styleUrl: './category-list.component.css',
 })
 export class CategoryListComponent implements OnInit {
   categories$!: Observable<any[]>;
-  id:any;
-  constructor(private categoryService: CategoryService,private route: ActivatedRoute) { 
-
-  }
+  id: any;
+  totalCategoriesCount: number = 0;
+  pageNumber: number = 1;
+  pageSize: number = 5;
+  categoriesCountList:number[] =[];
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.categories$ = this.categoryService.getCategories();
+    this.categoryService.getCategoriesCount().subscribe({
+      next: (count) => {
+        console.log('Total categories count:', count);
+        this.totalCategoriesCount = count;
+        this.categoriesCountList = new Array(
+          Math.ceil(this.totalCategoriesCount / this.pageSize));
+        this.categories$ = this.categoryService.getCategories(
+          undefined,
+          undefined,
+          undefined,
+          this.pageNumber,
+          this.pageSize
+        );
+      },
+      error: (err) => {
+        console.error('Error fetching categories count:', err);
+      },
+    });
   }
-  onDeleteCategory(id:string):void{
+  onDeleteCategory(id: string): void {
     this.categoryService.deleteCategory(id).subscribe({
       next: () => {
         alert('Category deleted successfully');
@@ -28,8 +50,53 @@ export class CategoryListComponent implements OnInit {
       error: (err) => {
         console.error('Error deleting category:', err);
         alert('Failed to delete category');
-      }
+      },
     });
   }
-
+  onSearch(filterQuery: string): void {
+    this.categories$ = this.categoryService.getCategories(filterQuery);
+  }
+  sort(sortBy: string, sortDirection: string): void {
+    this.categories$ = this.categoryService.getCategories(
+      undefined,
+      sortBy,
+      sortDirection
+    );
+  }
+  getPage(pageNumber: number): void {
+    this.pageNumber = pageNumber;
+    this.categories$ = this.categoryService.getCategories(
+      undefined,
+      undefined,
+      undefined,
+      this.pageNumber,
+      this.pageSize
+    );
+  }
+  getNextPage(): void {
+    if(this.pageNumber+1> this.categoriesCountList.length){
+      return;
+    }
+    this.pageNumber+=1;
+    this.categories$ = this.categoryService.getCategories(
+      undefined,
+      undefined,
+      undefined,
+      this.pageNumber,
+      this.pageSize
+    );
+  }
+  getPrevPage(): void {
+    if(this.pageNumber-1<1){
+      return;
+    }
+    this.pageNumber-=1;
+    this.categories$ = this.categoryService.getCategories(
+      undefined,
+      undefined,
+      undefined,
+      this.pageNumber,
+      this.pageSize
+    );
+  }
 }
--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -3,31 +3,46 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
   // apiUrl =`${environment.apiBaseUrl}Category/`;
   // private apiUrl = environment.apiBaseUrl + 'Category/';
   private apiUrl = 'https://localhost:7248/api/Category/';
 
+  constructor(private _http: HttpClient) {}
 
-  constructor(private _http:HttpClient) { }
-
-  addCategory(category:any):Observable<void>{
+  addCategory(category: any): Observable<void> {
     return this._http.post<void>(`${this.apiUrl}AddCategory`, category);
   }
-  getCategories():Observable<any[]>{
-    return this._http.get<any[]>(`${this.apiUrl}GetAllCategories`);
+  //https://localhost:7248/api/Category/GetAllCategories?sortBy=name&sortDirection=des
+  //https://localhost:7248/api/Category/GetAllCategories?pageNumber=1&pageSize=5
+  getCategories(
+    filterQuery?: string,
+    sortBy?: string,
+    sortDirection?: string,
+    pageNumber?: number,
+    pageSize?: number
+  ): Observable<any[]> {
+    const url = `${this.apiUrl}GetAllCategories?filterQuery=${filterQuery}&sortBy=${sortBy}&sortDirection=${sortDirection}
+    &pageNumber=${pageNumber}&pageSize=${pageSize}`;
+    console.log('URL:', url);
+    return this._http.get<any[]>(url);
   }
   getCategoryById(id: string): Observable<any> {
     return this._http.get<any>(`${this.apiUrl}GetCategoryById/${id}`);
   }
-  updateCategory(id:string, category: any): Observable<void> {
-    return this._http.put<void>(`${this.apiUrl}UpdateCategoryById/${id}`, category);
+  updateCategory(id: string, category: any): Observable<void> {
+    return this._http.put<void>(
+      `${this.apiUrl}UpdateCategoryById/${id}`,
+      category
+    );
   }
   deleteCategory(id: string): Observable<void> {
     return this._http.delete<void>(`${this.apiUrl}DeleteCategoryById/${id}`);
+  }
+  getCategoriesCount(): Observable<number> {
+    return this._http.get<number>(`${this.apiUrl}GetCategoriesCount`);
   }
 }
