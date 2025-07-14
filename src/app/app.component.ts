ac1307562b08e3b4c04568d8db990e846086f194
MounikaMalreddy <mounikamalreddy1998@gmail.com>
2025-07-14 21:23:08 +0530

    added loader 14072025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./core/components/navbar/navbar.component";
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, NgxUiLoaderModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,20 @@ import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxUiLoaderHttpModule, NgxUiLoaderModule, NgxUiLoaderRouterModule } from 'ngx-ui-loader';
 
 bootstrapApplication(AppComponent,{
  providers: [
   provideRouter(routes),
   provideHttpClient(), //registers HttpClientModule
-  importProvidersFrom(ReactiveFormsModule), //registers ReactiveFormsModule
+  importProvidersFrom(ReactiveFormsModule, //registers ReactiveFormsModule
+  BrowserAnimationsModule,
+  NgxUiLoaderModule,
+  NgxUiLoaderRouterModule.forRoot({showForeground:true}), // loader on route changes
+  NgxUiLoaderHttpModule.forRoot({
+    showForeground: true, // Show the loader on HTTP requests  
+  })) //registers NgxUiLoaderModule
  ]
 })
   .catch((err) => console.error(err));
